c1af71d86078d79d9440c7c3d6cb01ee52a02e7b
shashankthakre <shashank17892@gmail.com>
2023-02-18 12:25:05 +0530

    initial commit

---
+++ src/test/java/com/gitpractice/curdexample/CurdexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitpractice.curdexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurdexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
